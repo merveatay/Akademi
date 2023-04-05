8997bbdf4f601bb05373c99b7cdb71d872aaf94c
Merve nur Atay <mervenuratay@macbook-air.mynet>
2023-04-05 22:21:43 +0300

    Missing lines of code completed.

---
+++ firstWeek/degiskenler.py
@@ -0,0 +1,22 @@
+haber = "HABERINIZ OLSUN" #haber isimli, string türünde veri tutan değişken tanımlandı
+vade = 12
+#vade isimli integer tutan değişken tanımlandı
+faizOrani1 = 1.47
+#ondalıklı sayı için nokta kullanmına dikkat, virgül kullanılırsa veri tipi tuple olur
+print(type(faizOrani1))
+#type operatorü ile veri konsola yazdırıldı
+faizOrani2 = 1.44
+#değişken isminde sayı kullanılacaksa sayı başta olamaz
+mesaj = "Hoşgeldiniz"
+musteriAdi1 = "Merve"
+musteriSoyadi1 = "Atay"
+print(mesaj+" "+musteriAdi1+" "+musteriSoyadi1)
+#+ operatörü ile string concatenation yaptik. değişken print içinde kullanılırken "" kullanılmaz
+musteriAdi2 = "Ayşe"
+musteriSoyadi2 = "Öztürk"
+sonucMesaj = mesaj+" "+musteriAdi2+" "+musteriSoyadi2+"!"
+#verileri değişken içinde saklamak gereklidir
+print(sonucMesaj)
+sayi1 = 10
+sayi2= 20
+print(sayi1+sayi2)
